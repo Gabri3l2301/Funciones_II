fbd9082f52cbdde7fa5278ad72c92ad7842483e7
Gabriel <gpenalozag@unjbg.edu.pe>
2024-06-26 12:22:49 -0500

    Ampliando el ejercicio número 4 llamado CalcularRaices.cpp

--- CalcularRaices.cpp
+++ CalcularRaices.cpp
@@ -3,29 +3,23 @@
 #include <complex>
 using namespace std;
 
-void calcularRaices(double a, double b, double c) {
+void calcularRaices(double a, double b, double c, complex<double>& raiz1, complex<double>& raiz2, bool& sonReales) {
     double discriminante = pow(b, 2) - 4 * a * c;
-    if (discriminante > 0) {
-        double raiz1 = (-b + sqrt(discriminante)) / (2 * a);
-        double raiz2 = (-b - sqrt(discriminante)) / (2 * a);
-        cout << "Las raices son reales y diferentes." << endl;
-        cout << "Raiz 1: " << raiz1 << endl;
-        cout << "Raiz 2: " << raiz2 << endl;
-    } else if (discriminante == 0) {
-        double raiz = -b / (2 * a);
-        cout << "Las raices son reales e iguales." << endl;
-        cout << "Raiz: " << raiz << endl;
+    if (discriminante >= 0) {
+        sonReales = true;
+        raiz1 = (-b + sqrt(discriminante)) / (2 * a);
+        raiz2 = (-b - sqrt(discriminante)) / (2 * a);
     } else {
-        complex<double> raiz1 = (-b + sqrt(complex<double>(discriminante))) / (2.0 * a);
-        complex<double> raiz2 = (-b - sqrt(complex<double>(discriminante))) / (2.0 * a);
-        cout << "Las raices son complejas y diferentes." << endl;
-        cout << "Raiz 1: " << raiz1 << endl;
-        cout << "Raiz 2: " << raiz2 << endl;
+        sonReales = false;
+        raiz1 = (-b + sqrt(complex<double>(discriminante))) / (2.0 * a);
+        raiz2 = (-b - sqrt(complex<double>(discriminante))) / (2.0 * a);
     }
 }
 
 int main() {
     double coeficiente1, coeficiente2, coeficiente3;
+    complex<double> raiz1, raiz2;
+    bool sonReales;
     cout << "Introduce el coeficiente a: ";
     cin >> coeficiente1;
     cout << "Introduce el coeficiente b: ";
@@ -33,7 +27,16 @@ int main() {
     cout << "Introduce el coeficiente c: ";
     cin >> coeficiente3;
 
-    calcularRaices(coeficiente1, coeficiente2, coeficiente3);
+    calcularRaices(coeficiente1, coeficiente2, coeficiente3, raiz1, raiz2, sonReales);
 
+    if (sonReales) {
+        cout << "Las raices son reales." << endl;
+        cout << "Raiz 1: " << raiz1 << endl;
+        cout << "Raiz 2: " << raiz2 << endl;
+    } else {
+        cout << "Las raices son complejas." << endl;
+        cout << "Raiz 1: " << raiz1 << endl;
+        cout << "Raiz 2: " << raiz2 << endl;
+    }
     return 0;
 }
